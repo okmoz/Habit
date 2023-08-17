fa2ad8c0c368b3369100454fa675d340d6df07f9
okmoz <64868381+okmoz@users.noreply.github.com>
2023-08-17 18:21:02 +0300

    Add DevelopmentTests

--- HabitTests/DevelopmentTests.swift
+++ HabitTests/DevelopmentTests.swift
@@ -10,6 +10,36 @@ import XCTest
 @testable import Habit
 
 final class DevelopmentTests: BaseTestCase {
+    func testHabitInitialization() throws {
+        let habit = Habit(context: managedObjectContext, title: "Test Habit", motivation: "Test Motivation", color: .red)
+        XCTAssertNotNil(habit)
+        XCTAssertEqual(habit.title, "Test Habit")
+        XCTAssertEqual(habit.motivation, "Test Motivation")
+        XCTAssertEqual(habit.color, HabitColor.red)
+    }
+    
+    func testHabitExample() throws {
+        let exampleHabit = Habit.example
+        XCTAssertEqual(exampleHabit.title, "Example Habit")
+        XCTAssertEqual(exampleHabit.motivation, "Motivation text")
+        XCTAssertNotNil(exampleHabit.creationDate)
+        XCTAssertNotNil(exampleHabit.color)
+        XCTAssertFalse(exampleHabit.completedDates.isEmpty)
+    }
+    
+    func testHabitPropertySet() throws {
+        let habit = Habit(context: managedObjectContext, title: "Original Title", motivation: "Original Motivation", color: .green)
+        
+        habit.title = "New Title"
+        XCTAssertEqual(habit.title, "New Title")
+        
+        habit.motivation = "New Motivation"
+        XCTAssertEqual(habit.motivation, "New Motivation")
+        
+        habit.color = .yellow
+        XCTAssertEqual(habit.color, .yellow)
+    }
+    
     func testSampleDataCreationWorks() throws {
         try dataController.createSampleData()
         
@@ -21,15 +51,5 @@ final class DevelopmentTests: BaseTestCase {
         dataController.deleteAll()
         
         XCTAssertEqual(try managedObjectContext.count(for: Habit.fetchRequest()), 0, "deleteAll() should leave 0 items.")
-    }
-    
-    func testExampleHabitHasTitle() {
-        let habit = Habit.example
-        XCTAssertFalse(habit.title.isEmpty)
-    }
-    
-    func testExampleHabitHasCompletedDates() {
-        let habit = Habit.example
-        XCTAssertFalse(habit.completedDates.isEmpty)
     }
 }
